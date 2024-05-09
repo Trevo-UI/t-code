2e644cd8ed381bf6d148b72f6874bbbe16c23715
Gabriel Girardi <gabrielgirardi2811@gmail.com>
2024-05-09 15:02:25 -0300

    fix: 🐛 Corrigido vite env

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -19,7 +19,7 @@ function Contact() {
         };
 
         try {
-            const response = await fetch('http://localhost:3001/send-email', {
+            const response = await fetch(import.meta.env.VITE_SERVER_URL +  '/send-email', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
